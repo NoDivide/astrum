2c7187a4342404c22817ae8f591e0803e66db8b1
Ryan Taylor <ryan@nodividestudio.com>
2016-04-22 18:43:44 +0100

    Tweaked formatting and started working on active states.

--- assets/js/patterns.js
+++ assets/js/patterns.js
@@ -2,6 +2,26 @@ new Vue({
 
     el: 'html',
 
+    data: {
+        theme: {
+            brand_color: '#FEA1AC',
+            invert_text: true,
+            background_color: '#F4F4F4',
+            code_highlight_theme: 'solarized-dark'
+        },
+        assets: {
+            css: [],
+            js: []
+        },
+        font_libraries: {
+            typekit_code: null,
+            google_web_fonts: null,
+            typography_web_fonts: null
+        },
+        active: {},
+        open: {}
+    },
+
     scrollWatch: true,
 
     ready: function() {
@@ -12,14 +32,14 @@ new Vue({
 
         /**
          * Reads the patterns.json file.
-         *
-         * @return {undefined}
          */
         loadDataFile: function() {
             var _this = this;
 
             $.get('patterns.json', function(data) {
                 _this.$data = data;
+                _this.activeComponent = null;
+                _this.activeGroup = null;
                 _this.componentsLoaded = 0;
 
                 _this.loadIntro();
@@ -34,6 +54,11 @@ new Vue({
             });
         },
 
+        /**
+         * Update URL hash.
+         *
+         * @param hash
+         */
         updateHash: function(hash) {
             window.location.hash = hash;
         },
@@ -106,6 +131,7 @@ new Vue({
                         offsetBottom = nextEl === undefined ? $(_document).height() : $(nextEl).offset().top - 50;
 
                     if(_this.scrollWatch && $(_document).scrollTop() > $(this).offset().top - 50 && $(_document).scrollTop() < offsetBottom) {
+
                         $('.ndpl-nav__child-title').removeClass('active');
                         $('.ndpl-nav__child-title[href="#' + $(this).attr('id') + '"]').addClass('active');
 
@@ -119,9 +145,7 @@ new Vue({
         },
 
         /**
-         * Loads TypeKit
-         *
-         * @return {undefined}
+         * Loads TypeKit.
          */
         loadTypekit: function() {
             $.getScript('https://use.typekit.net/' + this.font_libraries.typekit_code + '.js', function() {
@@ -129,6 +153,9 @@ new Vue({
             });
         },
 
+        /**
+         * Load intro Markdown.
+         */
         loadIntro: function() {
             var _this = this;
 
@@ -137,12 +164,43 @@ new Vue({
             })
         },
 
+        /**
+         * Load scripts.
+         */
         loadScripts: function() {
             for (var i = 0; i < this.assets.js.length; i++) {
                 $('body').append('<script src="' + this.assets.js[i] + '"></script>');
             }
         },
 
+        /**
+         * Setup component groups.
+         */
+        setupGroups: function() {
+
+            // Loop through the groups
+            for (var i = 0; i < this.groups.length; i++) {
+                var group = this.groups[i];
+                group.open = false;
+                group.components = [];
+
+                for (var j = 0; j < this.components.length; j++) {
+                    if (this.components[j].group === group.name) {
+
+                        // Add html and description properties to the component object.
+                        this.$set('components[' + j + '].html', '');
+                        this.$set('components[' + j + '].description', '');
+                        this.$set('components[' + j + '].active', false);
+
+                        group.components.push(this.components[j]);
+                    }
+                }
+            }
+        },
+
+        /**
+         * Load group components.
+         */
         loadComponents: function() {
             for (var i = 0; i < this.groups.length; i++) {
                 for (var j = 0; j < this.groups[i].components.length; j++) {
@@ -153,15 +211,22 @@ new Vue({
             }
         },
 
+        /**
+         * Load component files.
+         *
+         * @param component
+         */
         loadComponent: function(component) {
             var _this = this;
 
+            // Get and set component markup
             $.get('components/' + component.group + '/' + component.name + '/markup.html', function(data) {
                 component.html = data;
             }).always(function() {
                 _this.incrementComponentsLoaded();
             });
 
+            // Get and set component description
             $.get('components/' + component.group + '/' + component.name + '/description.md', function(data) {
                 component.description = marked(data);
             }).always(function() {
@@ -169,44 +234,26 @@ new Vue({
             });
         },
 
+        /**
+         * Increment components loaded.
+         */
         incrementComponentsLoaded: function() {
             this.componentsLoaded += 1;
 
             if (this.componentsLoaded === this.components.length * 2) {
-                /**
-                 * Load custom JavaScripts
-                 */
+
+                 // Load custom JavaScripts
                 this.loadScripts();
 
-                /**
-                 * Initialize code highlighting
-                 */
+                // Initialize code highlighting
                 $('pre code').each(function(i, block) {
                     hljs.highlightBlock(block);
                 });
             }
         },
 
-        setupGroups: function() {
-            // Loop through the groups
-            for (var i = 0; i < this.groups.length; i++) {
-                var group = this.groups[i];
-                group.components = [];
+        setActiveComponent: function() {},
 
-                for (var j = 0; j < this.components.length; j++) {
-                    if (this.components[j].group === group.name) {
-
-                        // Add html and description properties to the component object.
-                        this.$set('components[' + j + '].html', '');
-                        this.$set('components[' + j + '].description', '');
-                        this.$set('components[' + j + '].id', this.components[j].name);
-
-                        group.components.push(this.components[j]);
-                    }
-                }
-            }
-        }
-
+        setOpenGroup: function() {}
     }
-
 });
