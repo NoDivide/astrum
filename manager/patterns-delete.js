c61ae9dbc82f83399ef6e56393aefb53ec0c3e4f
Ryan Taylor <ryan@nodividestudio.com>
2016-04-28 23:04:19 +0100

    Refactored delete command and prompt style.

--- manager/patterns-delete.js
+++ manager/patterns-delete.js
@@ -14,53 +14,67 @@ var group_name = program.args[0];
 if (group_name) {
     var parts = group_name.split('/'),
         componentIndex = utils.componentExists(group_name),
-        groupIndex = utils.groupExists(group_name);
+        groupIndex = utils.getGroupIndex(parts[0]);
 
     if(componentIndex !== false) {
         inquirer.prompt([
             {
+                type: 'confirm',
                 name: 'delete',
-                message: chalk.red('Are you sure you want to permanently delete the "' + parts[1] + '" component? (yes/no)')
+                message: function() {
+                    console.log();
+                    console.log(chalk.grey('Delete "' + parts[1] + '" component:'));
+                    console.log(chalk.grey('----------------------------------------------------------------'));
+                    return chalk.red('Are you sure you want to delete this component?');
+                },
+                default: false
             }
         ]).then(function (answers) {
-            if(answers.delete == 'no') {
+            if(!answers.delete) {
                 console.log();
                 console.log(chalk.grey('----------------------------------------------------------------'));
-                console.log(chalk.yellow('Deletion cancelled.'));
+                console.log(chalk.green('\u2713 Deletion cancelled.'));
+                console.log(chalk.grey('----------------------------------------------------------------'));
                 console.log();
                 return;
             }
 
-            if(answers.delete == 'yes') {
+            if(answers.delete) {
                 var componentCount = utils.getGroupComponentCount(parts[0]);
 
-                utils.$data.components.splice(componentIndex, 1);
+                utils.$data.groups[groupIndex].components.splice(componentIndex, 1);
                 utils.deleteComponentFiles(group_name);
 
                 if(componentCount == 1) {
                     inquirer.prompt([
                         {
-                            name: 'deleteGroup',
-                            message: chalk.red('Deleting this component will leave it\'s parent group empty. Delete the "' + parts[0] + '" group as well? (yes/no)')
+                            type: 'confirm',
+                            name: 'delete_group',
+                            message: chalk.red('Deleting this component will leave it\'s parent group empty. Delete the "' + parts[0] + '" group as well?'),
+                            default: true
                         }
                     ]).then(function (answers) {
 
-                        if(answers.deleteGroup == 'no') {
+                        if(!answers.delete_group) {
 
                             utils.saveData(function() {
+                                console.log();
                                 console.log(chalk.grey('----------------------------------------------------------------'));
-                                console.log(chalk.green('Component deleted successfully.'));
+                                console.log(chalk.green('\u2713 Component deleted successfully.'));
+                                console.log(chalk.grey('----------------------------------------------------------------'));
                                 console.log();
                             });
                         }
 
-                        if(answers.deleteGroup == 'yes') {
+                        if(answers.delete_group) {
                             utils.$data.groups.splice(groupIndex, 1);
                             utils.deleteGroupFolder(parts[0]);
 
                             utils.saveData(function () {
+                                console.log();
                                 console.log(chalk.grey('----------------------------------------------------------------'));
-                                console.log(chalk.green('Group and component deleted successfully.'));
+                                console.log(chalk.green('\u2713 Group and component deleted successfully.'));
+                                console.log(chalk.grey('----------------------------------------------------------------'));
                                 console.log();
                             });
                         }
@@ -68,8 +82,10 @@ if (group_name) {
                 } else {
 
                     utils.saveData(function() {
+                        console.log();
                         console.log(chalk.grey('----------------------------------------------------------------'));
-                        console.log(chalk.green('Component deleted successfully.'));
+                        console.log(chalk.green('\u2713 Component deleted successfully.'));
+                        console.log(chalk.grey('----------------------------------------------------------------'));
                         console.log();
                     });
                 }
