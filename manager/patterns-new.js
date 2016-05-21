f4fc3d3b261e7d3e8d88bdef989cb83b2322d10f
Ryan Taylor <ryan@nodividestudio.com>
2016-05-21 19:20:57 +0100

    Added facility to create components of type colors

--- manager/patterns-new.js
+++ manager/patterns-new.js
@@ -8,6 +8,7 @@ var program = require('commander'),
 utils.init();
 
 program
+    .option('-t, --type <name>', 'set component type: (standard|colors). Default standard.')
     .parse(process.argv);
 
 if (!process.argv.slice(2).length) {
@@ -43,6 +44,9 @@ if (group_name) {
                 }
             },
             {
+                when: function() {
+                    return program.type && program.type !== 'colors';
+                },
                 type: 'list',
                 name: 'width',
                 message: function() {
@@ -60,8 +64,14 @@ if (group_name) {
                 ]
             }
         ]).then(function (answers) {
+            var typeColor =  program.type && program.type == 'colors';
+
             newComponent.title = answers.title;
             if(answers.width != 'full') newComponent.width = answers.width;
+            if(typeColor) {
+                newComponent.type = 'colors';
+                newComponent.colors = [];
+            }
 
             // If new group prompt for new group details
             if (!utils.groupExists(group_name)) {
@@ -89,8 +99,8 @@ if (group_name) {
                     }
                 ]).then(function (answers) {
                     newGroup.name = parts[0];
-                    newGroup.title = answers.title;;
-                    
+                    newGroup.title = answers.title;
+
                     utils.$data.groups.splice(answers.group_position, 0, newGroup);
                     utils.$data.groups[answers.group_position].components = [newComponent];
 
@@ -100,10 +110,39 @@ if (group_name) {
                             console.log(chalk.grey('----------------------------------------------------------------'));
                             console.log(chalk.green('\u2713 Pattern library data saved successfully.'));
                             console.log(chalk.grey('----------------------------------------------------------------'));
-                            console.log();
-                            console.log(chalk.yellow('Add your component markup to /components/' + newComponent.group + '/' + newComponent.name + '/markup.html'));
-                            console.log(chalk.yellow('Add your component description to /components/' + newComponent.group + '/' + newComponent.name + '/description.md (Markdown supported)'));
-                            console.log();
+
+                            if(typeColor) {
+                                console.log();
+                                console.log(chalk.yellow('Ignore /components/' + newComponent.group + '/' + newComponent.name + '/markup.html'));
+                                console.log(chalk.yellow('Add your component description to /components/' + newComponent.group + '/' + newComponent.name + '/description.md (Markdown supported)'));
+                                console.log();
+                                console.log(chalk.yellow('Locate this component in your data.json file and add your colors to the "colors"'));
+                                console.log(chalk.yellow('array that has been created for you. Colors must be hex values e.g.:'));
+                                console.log();
+                                console.log(chalk.yellow('"colors": ['));
+                                console.log(chalk.yellow('    "#4c4c4c"'));
+                                console.log(chalk.yellow('    "#7d8284"'));
+                                console.log(chalk.yellow('    "#a6b1b5"'));
+                                console.log(chalk.yellow('    "#e6eaf2"'));
+                                console.log(chalk.yellow('    "#FFFFFF"'));
+                                console.log(chalk.yellow(']'));
+                                console.log();
+                                console.log(chalk.yellow('You can also add complimentary colors by comma separating the values e.g.:'));
+                                console.log();
+                                console.log(chalk.yellow('"colors": ['));
+                                console.log(chalk.yellow('    "#7da9f9,#507ed3"'));
+                                console.log(chalk.yellow('    "#f469a7,#c14c80"'));
+                                console.log(chalk.yellow('    "#60ceb8,#3fa18d"'));
+                                console.log(chalk.yellow('    "#f5d13f,#f5a63f"'));
+                                console.log(chalk.yellow('    "#e199e5,#c776cb"'));
+                                console.log(chalk.yellow(']'));
+                                console.log();
+                            } else {
+                                console.log();
+                                console.log(chalk.yellow('Add  your component markup to /components/' + newComponent.group + '/' + newComponent.name + '/markup.html'));
+                                console.log(chalk.yellow('Add your component description to /components/' + newComponent.group + '/' + newComponent.name + '/description.md (Markdown supported)'));
+                                console.log();
+                            }
                         });
                     }
                 });
@@ -129,10 +168,39 @@ if (group_name) {
                             console.log(chalk.grey('----------------------------------------------------------------'));
                             console.log(chalk.green('\u2713 Pattern library data saved successfully.'));
                             console.log(chalk.grey('----------------------------------------------------------------'));
-                            console.log();
-                            console.log(chalk.yellow('Add your component markup to /components/' + newComponent.group + '/' + newComponent.name + '/markup.html'));
-                            console.log(chalk.yellow('Add your component description to /components/' + newComponent.group + '/' + newComponent.name + '/description.md (Markdown supported)'));
-                            console.log();
+
+                            if(typeColor) {
+                                console.log();
+                                console.log(chalk.yellow('Ignore /components/' + newComponent.group + '/' + newComponent.name + '/markup.html'));
+                                console.log(chalk.yellow('Add your component description to /components/' + newComponent.group + '/' + newComponent.name + '/description.md (Markdown supported)'));
+                                console.log();
+                                console.log(chalk.yellow('Locate this component in your data.json file and add your colors to the "colors"'));
+                                console.log(chalk.yellow('array that has been created for you. Colors must be hex values e.g.:'));
+                                console.log();
+                                console.log(chalk.yellow('"colors": ['));
+                                console.log(chalk.yellow('    "#4c4c4c"'));
+                                console.log(chalk.yellow('    "#7d8284"'));
+                                console.log(chalk.yellow('    "#a6b1b5"'));
+                                console.log(chalk.yellow('    "#e6eaf2"'));
+                                console.log(chalk.yellow('    "#FFFFFF"'));
+                                console.log(chalk.yellow(']'));
+                                console.log();
+                                console.log(chalk.yellow('You can also add complimentary colors by comma separating the values e.g.:'));
+                                console.log();
+                                console.log(chalk.yellow('"colors": ['));
+                                console.log(chalk.yellow('    "#7da9f9,#507ed3"'));
+                                console.log(chalk.yellow('    "#f469a7,#c14c80"'));
+                                console.log(chalk.yellow('    "#60ceb8,#3fa18d"'));
+                                console.log(chalk.yellow('    "#f5d13f,#f5a63f"'));
+                                console.log(chalk.yellow('    "#e199e5,#c776cb"'));
+                                console.log(chalk.yellow(']'));
+                                console.log();
+                            } else {
+                                console.log();
+                                console.log(chalk.yellow('Add  your component markup to /components/' + newComponent.group + '/' + newComponent.name + '/markup.html'));
+                                console.log(chalk.yellow('Add your component description to /components/' + newComponent.group + '/' + newComponent.name + '/description.md (Markdown supported)'));
+                                console.log();
+                            }
                         });
                     }
                 });
